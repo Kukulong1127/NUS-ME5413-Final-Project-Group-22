21172dbad19b0e6915388dea3b526d0c6a0c11fa
Kukulong1127 <e1101661@u.nus.edu>
2023-04-03 01:01:58 +0800

    Upload the final version of the project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/charles/ME5413_Final_Project/src
+# Build directory: /home/charles/ME5413_Final_Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("jackal_description")
+subdirs("interactive_tools")
+subdirs("me5413_world")
